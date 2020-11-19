81f428a09907095160393d70b87645075d14ddef
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-19 20:53:29 +0100

    Added name and age to Person props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person />
+        <Person name='Derpine' age='23' />
+        <Person name='Derper' age='69'>My hobbies: eating</Person>
+        <Person name='Derpadette' age='420' />
       </div>
     );
     // return createElement('div', {className: 'App'}, createElement('h1', null, 'Does this work now?'))
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,6 @@
 
-const Person = () => {
-  return (<p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>);
+const Person = (props) => {
+  return (<p>I'm {props.name} and I am {props.age} years old!</p>);
 };
 
 export default Person;
