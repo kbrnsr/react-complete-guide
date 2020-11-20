3f33ff00e9642c5ad20a4b95503418a397fef4b2
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-20 19:24:33 +0100

    Removed styled-components and all references to it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import { Component } from 'react';
-import styled from 'styled-components';
 import './App.css';
 import Person from './components/Person/Person'
-
-const StyledButton = styled.button`
-  background-color: ${props => {
-    return props.showPersonsBool ? 'red' : 'green';
-  }};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  
-  &:hover {
-    background-color: ${props => {
-      return props.showPersonsBool ? 'salmon' : 'lightgreen';
-    }};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -89,11 +70,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working</p>
-        <StyledButton
-          showPersonsBool={showPersons}
+        <button
+          className='button'
           onClick={this.togglePersonsHandler} >
           Toggle Persons
-        </StyledButton>
+        </button>
         {renderPersons}
       </div>
     );
