ed33af01717f8dd121b1655dc24bd71f4e9f5f7b
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-21 13:26:34 +0100

    Refactored Persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,12 +1,19 @@
 import Person from './Person/Person';
 
-const Persons = (props) => props.persons.map((person, index) => {
-    return <Person
-      click={() => props.clicked(index)} 
-      name={person.name}
-      age={person.age}
-      key={person.id}
-      changed={(event) => props.changed(event, person.id)} />
-  });
+const Persons = (props) => {
+  return (
+    props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => props.clicked(index)} 
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => props.changed(event, person.id)}
+        />
+      );
+    })
+  );
+};
 
 export default Persons;
